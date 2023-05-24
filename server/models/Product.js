3bbfca51e748d89f7013992d23e3b6462bfb3aa6
Saar <saarphone@gmail.com>
2023-05-24 17:06:08 +0300

    backend

---
+++ server/models/Product.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+    name:{type:String,required : true},
+    rating:{type:Number,required : true},
+    comment:{type:String,required : true},
+    title:{type:String,required : true},
+    user:{type:mongoose.SchemaTypes.ObjectId,required:true,ref:'User'},
+},{timestamps:true}
+);
+
+const productSchema =new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    brand:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    reviews:{reviewSchema},
+    rating:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    numberOfReviews:{
+        type:Number,
+        required:true,
+    },
+    price:{
+        type:String,
+        required:true,
+        default:0,
+    },
+    stock:{
+        type:String,
+        required:true,
+        default:0,
+    },
+    productIsNew:{
+        type:Boolean,
+        default:false,
+    },
+
+
+},{timestamps:true});
+
+const Product = mongoose.model('Product',productSchema);
+export default Product; 
